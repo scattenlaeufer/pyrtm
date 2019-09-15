372393520960a2686000e0bb3626450659620138
Björn Guth <scatty@bguth.de>
2019-09-15 19:24:27 +0200

    Add documentation to utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,23 @@
+"""
+Collection of utility functions
+"""
+
+
 def calculate_dos_rt(value, roll):
+    """
+    Calculate degrees of success based on Rogue Trader
+
+    :param value int: Value against which the test is rolled
+    :param roll int: Result of the roll
+    """
     return abs(roll - value) // 10
 
 
 def calculate_dos_dh2(value, roll):
+    """
+    Calculate degrees of success based on Dark Heresy 2
+
+    :param value int: Value against which the test is rolled
+    :param roll int: Result of the roll
+    """
     return 1 + abs(roll // 10 - value // 10)
